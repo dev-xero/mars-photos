9a49411b13ba1d923bd4852b29ba2f1eefb240d1
Xero kun <elo48858@gmail.com>
2023-02-24 14:54:46 +0100

    `✨UPDATE` Created Loading and Error Screens

---
+++ app/src/main/java/dev/xero/marsphotos/ui/screens/LoadingScreen.kt
@@ -0,0 +1,22 @@
+package dev.xero.marsphotos.ui.screens
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import dev.xero.marsphotos.R
+
+@Composable
+fun LoadingScreen(
+	modifier: Modifier = Modifier
+) {
+	Box(
+		modifier = modifier.fillMaxSize(),
+		contentAlignment = Alignment.Center
+	) {
+		Text(text = stringResource(id = R.string.loading))
+	}
+}
