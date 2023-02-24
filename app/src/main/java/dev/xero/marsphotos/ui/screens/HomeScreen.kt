6420470d006736f9c1aa307c8f7b66c0d420686d
Xero kun <elo48858@gmail.com>
2023-02-24 17:23:54 +0100

    `✨UPDATE` Updated Drawables

--- app/src/main/java/dev/xero/marsphotos/ui/screens/HomeScreen.kt
+++ app/src/main/java/dev/xero/marsphotos/ui/screens/HomeScreen.kt
@@ -6,10 +6,6 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import dev.xero.marsphotos.R
-import dev.xero.marsphotos.ui.theme.MarsPhotosTheme
 
 @Composable
 fun HomeScreen(
